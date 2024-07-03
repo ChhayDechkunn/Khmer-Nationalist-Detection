e4dc88495f678bb982a8ffa4718675a3a7de7329
unknown <chhay.dechkunn@gmail.com>
2024-07-03 10:43:53 +0200

    custom unpickler

--- gunicorn.py
+++ gunicorn.py
@@ -3,22 +3,26 @@
 import os
 import sys
 
-def post_fork(server, worker):
-    # Adding the project directory to sys.path
-    project_dir = os.path.dirname(os.path.abspath(__file__))
-    sys.path.append(project_dir)
+def remove_affixes(khmer_word, affixes):
+    for affix in affixes:
+        if khmer_word.startswith(affix):
+            khmer_word = khmer_word[len(affix):]
+        if khmer_word.endswith(affix):
+            khmer_word = khmer_word[:-len(affix)]
+    return khmer_word
 
-    # Importing the function
-    global tokenize_kh, remove_affixes
-    from preprocessing import tokenize_kh, remove_affixes
-
-    # Log to ensure the function is imported
-    server.log.info("tokenize_kh function imported successfully in worker.")
-
-bind = "0.0.0.0:8000"
-workers = 4
-threads = 2
-timeout = 120
-
-# Adding the post_fork hook
-post_fork = post_fork
+def tokenize_kh(text, stopwords=khmer_stopword):
+    # Tokenize the input text
+    tokens_pos = khmernltk.pos_tag(text)
+    
+    # Filter out tokens with English characters, numerals, or punctuation, stopwords, and certain POS tags
+    khmer_words = [
+        remove_affixes(word, affixes) for word, pos in tokens_pos if (
+            not re.search("[a-zA-Z0-9!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]៖«»។៕!-…", word)
+            and word not in stopwords
+            and not any(char in word for char in '១២៣៤៥៦៧៨៩០')
+            and pos not in ['o','.','1']
+        )
+    ]
+    
+    return khmer_words
